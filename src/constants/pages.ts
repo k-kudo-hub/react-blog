91f32b88a72921bf8b34447e14811571438c0a9f
k-kudo-hub <tongariisaibou@gmail.com>
2022-08-20 21:18:48 +0900

    :hammer: Defined constants for sites and pages.

---
+++ src/constants/pages.ts
@@ -0,0 +1,9 @@
+const PAGES = {
+  HOME: {
+    TITLE: "ホーム",
+    DESCRIPTION: "ホームです",
+    PATH: "/",
+  },
+};
+
+export default PAGES;
---
+++ src/constants/siteInfo.ts
@@ -0,0 +1,6 @@
+const SITE_INFO = {
+  TITLE: "Cut Blog",
+  DESCRIPTION: "",
+};
+
+export default SITE_INFO;
