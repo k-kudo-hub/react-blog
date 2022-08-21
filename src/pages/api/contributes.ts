5f011b4f7373f38169462ed1847a7dc316a8af19
k-kudo-hub <tongariisaibou@gmail.com>
2022-08-21 09:19:29 +0900

    :lipstick: :necktie: [WIP] Developing top page UI, API.

---
+++ src/pages/api/contributes.ts
@@ -0,0 +1,26 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getAllContributes } from "../../server/usecase/contribute";
+
+interface contribute {
+  id: number;
+  title: string;
+  tags: string[];
+}
+
+type ResponseData = {
+  contributes: contribute[];
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+  if (req.method === "GET") {
+    try {
+      const contributes = getAllContributes();
+      res.status(200).json({ contributes });
+    } catch (e) {
+      res.status(500);
+    }
+  }
+}
