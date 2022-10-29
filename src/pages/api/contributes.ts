96042bc1cfe144e045db3dce623eb7d76b8a474c
k-kudo-hub <tongariisaibou@gmail.com>
2022-10-29 23:42:57 +0900

    :hammer: The status code was set as a constant.

--- src/pages/api/contributes.ts
+++ src/pages/api/contributes.ts
@@ -1,22 +1,27 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import STATUS_CODES from "@constants/errors/statusCode";
+import { ContributeType } from "src/server/domain/entity/contribute";
 import { getAllContributes } from "../../server/usecase/contribute";
-import { ContributeFormatted } from "../../server/domain/model/contribute";
 
 type ResponseData = {
-  contributes: ContributeFormatted[];
+  contributes: ContributeType[];
 };
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
-  if (req.method === "GET") {
-    try {
+  /**
+   * TODO
+   * 各apiごとにreq.methodを書くのは保守性が悪いので共通化する
+   */
+  try {
+    if (req.method === "GET") {
       const contributes = await getAllContributes();
-      res.status(200).json({ contributes });
-    } catch (e) {
-      console.error(e);
-      res.status(500).end();
+      res.status(STATUS_CODES.OK).json({ contributes });
     }
+  } catch (e) {
+    console.error(e.stack);
+    res.status(e.statusCode || STATUS_CODES.INTERNAL_SERVER_ERROR).end();
   }
 }
