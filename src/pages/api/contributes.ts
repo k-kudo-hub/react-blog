4ed9837a8e7900c63e08de319d56d9864fab5ac8
k-kudo-hub <tongariisaibou@gmail.com>
2022-12-30 16:32:42 +0900

    :hammer: Replace with httpMethodHandler

--- src/pages/api/contributes.ts
+++ src/pages/api/contributes.ts
@@ -1,27 +1,19 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import STATUS_CODES from "@constants/errors/statusCode";
-import { getAllContributes } from "../../server/usecase/contribute";
-import ContributeEntity from "@server/domain/entity/contribute";
-
-type ResponseData = {
-  contributes: ContributeEntity[];
-};
+import { getAllContributes } from "@server/usecase/contribute";
+import {
+  HttpMethodHandler,
+  ResponseData,
+} from "@server/presentation/middleware/httpMethodHandler";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
-  /**
-   * TODO
-   * 各apiごとにreq.methodを書くのは保守性が悪いので共通化する
-   */
-  try {
-    if (req.method === "GET") {
-      const contributes = await getAllContributes();
-      res.status(STATUS_CODES.OK).json({ contributes });
-    }
-  } catch (e) {
-    console.error(e.stack);
-    res.status(e.statusCode || STATUS_CODES.INTERNAL_SERVER_ERROR).end();
-  }
+  const methodHandler = new HttpMethodHandler({
+    get: async () => {
+      return await getAllContributes();
+    },
+  });
+
+  await methodHandler.execute(req, res);
 }
