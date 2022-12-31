ce62b9dfeba4ca2c96ac4f1343c47f7af23d7614
k-kudo-hub <tongariisaibou@gmail.com>
2022-12-31 12:11:25 +0900

    :children_crossing: Fixed unfriendly date notation.

--- src/server/domain/factory/contribute/index.ts
+++ src/server/domain/factory/contribute/index.ts
@@ -1,10 +1,11 @@
-import { BaseFactory } from "../base";
+import dayjs from "dayjs";
 import ContributeEntity from "@server/domain/entity/contribute";
 import {
   ContributeType,
   ContributeDataType,
   ContributeTagRelationDataType,
 } from "@server/domain/entity/contribute/types";
+import { BaseFactory } from "../base";
 
 export class ContributeFactory extends BaseFactory {
   create(params: ContributeType): ContributeEntity {
@@ -19,8 +20,16 @@ export class ContributeFactory extends BaseFactory {
         param.tags?.map(
           (tagRelation: ContributeTagRelationDataType) => tagRelation.tag
         ) || [];
+      const publishedAt = param.publishedAt
+        ? dayjs(param.publishedAt).format("YYYY年MM月DD日")
+        : null;
+      const lastEditedAt = param.lastEditedAt
+        ? dayjs(param.lastEditedAt).format("YYYY年MM月DD日")
+        : null;
       return {
         ...param,
+        publishedAt,
+        lastEditedAt,
         tags,
       };
     });
