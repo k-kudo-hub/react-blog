6aea35e0b75f7e18e36a296c20b5906320d14328
k-kudo-hub <tongariisaibou@gmail.com>
2023-01-04 23:40:15 +0900

    :lipstick: Created UI for submission details page.

---
+++ src/client/containers/contributes/Detail/index.tsx
@@ -0,0 +1,77 @@
+import { NextPage } from "next";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+
+import DoubleLineTemplate from "@components/templates/DoubleLineTemplate";
+import { Contribute as ContributeType } from "../../../models/contribute";
+import { ContributeInterface } from "../../../interface/contributes";
+import styles from "./style.module.scss";
+import CodeBlock from "./CodeBlock";
+import Image from "next/image";
+import IMAGE_PATH from "src/client/styles/images";
+
+const contributeInterface = new ContributeInterface();
+
+const ContributeDetail: NextPage = () => {
+  const router = useRouter();
+  const [contribute, setContribute] = useState<ContributeType>();
+
+  useEffect(() => {
+    if (router.query.identityCode) {
+      fetchContribute(router.query.identityCode as string);
+    }
+  }, [router.query]);
+
+  const fetchContribute = async (identityCode: string) => {
+    const contribute = await contributeInterface.getContribute(identityCode);
+    setContribute(contribute);
+  };
+
+  return (
+    <DoubleLineTemplate pageTitle={contribute?.title}>
+      <div className={styles.detailContainer}>
+        <section className={styles.detailHeader}>
+          <div className={styles.userInfoContainer}>
+            <div className={styles.userInfo}>
+              <div className={styles.userIcon}>
+                <Image src={IMAGE_PATH.FIRE_ICON} width={30} height={30} />
+              </div>
+              <span>@{contribute?.user.nickName}</span>
+            </div>
+            <div className={styles.publishedAtContainer}>
+              {contribute?.publishedAt ? (
+                <span>公開日: {contribute?.publishedAt}</span>
+              ) : null}
+              {contribute?.lastEditedAt ? (
+                <span>更新日: {contribute?.lastEditedAt}</span>
+              ) : null}
+            </div>
+          </div>
+          <h1 className={styles.title}>{contribute?.title}</h1>
+          <p>
+            {contribute?.tags?.map((tag) => (
+              <span className={styles.tag} key={tag.id}>
+                # {tag.name}
+              </span>
+            ))}
+          </p>
+        </section>
+        <section className={styles.detailBody}>
+          <div className={styles.markdownContainer}>
+            <ReactMarkdown
+              remarkPlugins={[remarkGfm]}
+              components={{
+                code: CodeBlock,
+              }}
+            >
+              {contribute?.content as string}
+            </ReactMarkdown>
+          </div>
+        </section>
+      </div>
+    </DoubleLineTemplate>
+  );
+};
+export default ContributeDetail;
