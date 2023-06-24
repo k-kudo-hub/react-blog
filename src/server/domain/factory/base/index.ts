c7855ec50ab46407d9772ac0aa3bca473fc2b655
WAKO@program <tongarisaibou@gmail.com>
2023-06-24 19:28:11 +0900

    :hammer: Fix deprecated methods.

--- src/server/domain/factory/base/index.ts
+++ src/server/domain/factory/base/index.ts
@@ -1,14 +1,14 @@
-import { plainToClass } from "class-transformer";
+import { plainToInstance } from "class-transformer";
 
 export abstract class BaseFactory {
   protected createEntity<E, P>(entity: any, plainObject: P): E {
-    return plainToClass(entity, plainObject, {
+    return plainToInstance(entity, plainObject, {
       excludeExtraneousValues: true,
     }) as E;
   }
 
   protected createEntityArray<E, P>(entity: any, plainObjectArray: P[]): E[] {
-    return plainToClass(entity, plainObjectArray, {
+    return plainToInstance(entity, plainObjectArray, {
       excludeExtraneousValues: true,
     });
   }
