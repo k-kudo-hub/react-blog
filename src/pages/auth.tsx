47a940aabf4f3ad679fe43f2ca036f49aa624314
k-kudo-hub <tongariisaibou@gmail.com>
2024-02-04 11:32:05 +0900

    :recycle: マジックストリングの排除

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -3,6 +3,12 @@ import { useEffect } from "react";
 import Me from "src/client/models/me";
 import useMeState from "src/client/state/me";
 
+const AUTH_STATUS = {
+  AUTHENTICATED: "authenticated",
+  UNAUTHENTICATED: "unauthenticated",
+  LOADING: "loading",
+} as const;
+
 /**
  * ユーザー情報を取得してstateに保存するためのコンポーネント
  * 基本的にここ以外ではsetMeを使わないことにしたい
@@ -12,7 +18,7 @@ const Auth = () => {
   const { status, data } = useSession();
 
   useEffect(() => {
-    if (status === "authenticated") {
+    if (status === AUTH_STATUS.AUTHENTICATED) {
       const user = {
         ...data.user,
         isLoggedIn: true,
