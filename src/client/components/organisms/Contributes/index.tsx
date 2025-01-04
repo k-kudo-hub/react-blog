da0f33c76c3d02b6a4bdda4e7136a5742f5a072f
WAKO@program <tongarisaibou@gmail.com>
2025-01-04 21:20:01 +0900

    :recycle: コンポーネントにドメイン情報を持たせない

--- src/client/components/organisms/Contributes/index.tsx
+++ src/client/components/organisms/Contributes/index.tsx
@@ -1,18 +1,21 @@
 import styles from "./style.module.scss";
-import LastEditedAt from "@components/atoms/Texts/LastEditedAt";
-import Draft from "@components/atoms/Texts/Draft";
 import Tag from "@components/atoms/Tags";
 import Card from "@components/molecules/Card";
 import { Tag as TagType } from "../../../models/tag";
 import { Contribute as ContributeType } from "../../../models/contribute";
 import { CONTRIBUTE_STATUS } from "@server/domain/entity/contribute";
+import TextWithIcon from "@components/atoms/Texts/TextWithIcon";
 
-// TODO: componentsがドメイン情報を持つべきではない
 const createPublicStatusElement = (contribute: ContributeType): JSX.Element => {
-  return contribute.status === CONTRIBUTE_STATUS.PUBLISHED ? (
-    <LastEditedAt lastEditedAt={contribute?.lastEditedAt} />
+  return contribute.status === CONTRIBUTE_STATUS.PUBLISHED &&
+    !!contribute.publishedAt ? (
+    <TextWithIcon
+      iconName="feather-pen.svg"
+      iconAlt="公開"
+      text={contribute.publishedAt}
+    />
   ) : (
-    <Draft />
+    <TextWithIcon iconName="lock.svg" iconAlt="下書き" text="下書き" />
   );
 };
 
