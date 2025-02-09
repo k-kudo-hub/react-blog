21841ca1958cd39182a58d8b087dd6a14c3f25b7
WAKO@program <tongarisaibou@gmail.com>
2025-02-09 15:03:13 +0900

    :chart_with_upwards_trend: GA4を導入

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import Auth from "./auth";
 import { FlashMessageProvider } from "@components/atoms/Flash";
 import SessionProvider from "@/client/providers/SessionProvider";
 import { getBaseUrl } from "@/common/utils/url";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export default function RootLayout({
   children,
@@ -13,9 +14,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   const baseUrl = getBaseUrl();
+  const gaId = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID || "";
   return (
     <html lang="ja">
       <head>
+        {gaId ? <GoogleAnalytics gaId={gaId} /> : null}
         <link
           rel="alternate"
           type="application/atom+xml"
